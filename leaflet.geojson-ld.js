c05b92cd9ca2e02d7235e10c6c0ce56bd4f3777f
Sean Gillies <sean.gillies@gmail.com>
2014-06-22 10:47:36 -0600

    Remove initialize function.

--- leaflet.geojson-ld.js
+++ leaflet.geojson-ld.js
@@ -1,10 +1,6 @@
-L.GeoJSON_LD = L.GeoJSON.extend({
-    initialize: function (options) {
-        L.GeoJSON.prototype.initialize.call(this, {features:[]}, options);
-    }
-});
+L.GeoJSONLD = L.GeoJSON.extend({});
 
-L.geoJsonLd = function(a,b){
-    return new L.GeoJSON_LD(a,b);
+L.geoJsonLd = function(){
+    return new L.GeoJSONLD();
 }
 
