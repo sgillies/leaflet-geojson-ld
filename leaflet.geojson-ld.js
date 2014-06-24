44207ec5f35bbb39fa6b1ec0724714685c378525
Sean Gillies <sean.gillies@gmail.com>
2014-06-23 22:16:08 -0600

    Extend L.GeoJSON properly this time.
    Well, that's the intention.

--- leaflet.geojson-ld.js
+++ leaflet.geojson-ld.js
@@ -1,6 +1,39 @@
-L.GeoJSONLD = L.GeoJSON.extend({});
+L.GeoJSONLD = L.GeoJSON.extend({
 
-L.geoJsonLd = function(){
-    return new L.GeoJSONLD();
+    addData: function (geojson) {
+        var features = L.Util.isArray(geojson) ? geojson : geojson.features,
+            i, len, feature;
+
+        if (features) {
+            for (i = 0, len = features.length; i < len; i++) {
+                // Only add this if geometry or geometries are set and not null
+                feature = features[i];
+                if (feature.geometries || feature.geometry || feature.features || feature.coordinates) {
+                    this.addData(feature);
+                }
+            }
+            return this;
+        }
+
+        var options = this.options;
+
+        if (options.filter && !options.filter(geojson)) { return; }
+
+        var layer = L.GeoJSON.geometryToLayer(geojson, options);
+        layer.feature = L.GeoJSON.asFeature(geojson);
+
+        layer.defaultOptions = layer.options;
+        this.resetStyle(layer);
+
+        if (options.onEachFeature) {
+            options.onEachFeature(geojson, layer);
+        }
+
+        return this.addLayer(layer);
+    },
+});
+
+L.geoJsonLd = function(data, options){
+    return new L.GeoJSONLD(data, options);
 }
 
